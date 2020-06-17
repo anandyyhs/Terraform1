992ca199b36142f963ed0d75e8f91d587a83d6ff
anandyyhs <67015472+anandyyhs@users.noreply.github.com>
2020-06-17 15:47:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 	print "\n";
 	$url = file_get_contents("link.txt");
 	$url_arr = explode("\n" , $url);
-	echo "<img src = '" . $url_arr[$0] . "' alt='image' width='300px' height='200px'>";
+	echo "<img src = '" . $url_arr[0] . "' alt='image' width='300px' height='200px'>";
 
 ?>
